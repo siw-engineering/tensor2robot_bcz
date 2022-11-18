3d07f29ea0724f8b086dd0edfc8dd1568a36756f
cjpurackal <christeejacobs@gmail.com>
2022-11-18 11:41:11 +0530

    restructure

---
+++ tensor2robot/export_generators/default_export_generator_test.py
@@ -0,0 +1,157 @@
+# coding=utf-8
+# Copyright 2022 The Tensor2Robot Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for tensor2robot.export_generators.default_export_generator."""
+
+from absl.testing import parameterized
+import numpy as np
+from tensor2robot.export_generators import default_export_generator
+from tensor2robot.preprocessors import noop_preprocessor
+from tensor2robot.utils import mocks
+import tensorflow.compat.v1 as tf
+from tensorflow.compat.v1 import estimator as tf_estimator
+from tensorflow.contrib import predictor as contrib_predictor
+from tensorflow.contrib import tpu as contrib_tpu
+
+MAX_STEPS = 4000
+BATCH_SIZE = 32
+
+
+class DefaultExportGeneratorTest(tf.test.TestCase, parameterized.TestCase):
+
+  def _train_and_eval_reference_model(self, path, multi_dataset=False):
+    model_dir = self.create_tempdir().full_path
+    mock_t2r_model = mocks.MockT2RModel(
+        preprocessor_cls=noop_preprocessor.NoOpPreprocessor,
+        multi_dataset=multi_dataset)
+
+    # We create a tpu estimator for potential training.
+    estimator = contrib_tpu.TPUEstimator(
+        model_fn=mock_t2r_model.model_fn,
+        use_tpu=mock_t2r_model.is_device_tpu,
+        config=contrib_tpu.RunConfig(model_dir=model_dir),
+        train_batch_size=BATCH_SIZE,
+        eval_batch_size=BATCH_SIZE)
+
+    mock_input_generator = mocks.MockInputGenerator(batch_size=BATCH_SIZE,
+                                                    multi_dataset=multi_dataset)
+    mock_input_generator.set_specification_from_model(
+        mock_t2r_model, tf_estimator.ModeKeys.TRAIN)
+
+    # We optimize our network.
+    estimator.train(
+        input_fn=mock_input_generator.create_dataset_input_fn(
+            mode=tf_estimator.ModeKeys.TRAIN),
+        max_steps=MAX_STEPS)
+
+    # Verify that the serving estimator does exactly the same as the normal
+    # estimator with all the parameters.
+    estimator_predict = tf_estimator.Estimator(
+        model_fn=mock_t2r_model.model_fn,
+        config=tf_estimator.RunConfig(model_dir=model_dir))
+
+    prediction_ref = estimator_predict.predict(
+        input_fn=mock_input_generator.create_dataset_input_fn(
+            mode=tf_estimator.ModeKeys.EVAL))
+
+    return model_dir, mock_t2r_model, prediction_ref
+
+  def test_create_serving_input_receiver_numpy(self):
+    (model_dir, mock_t2r_model,
+     prediction_ref) = self._train_and_eval_reference_model('numpy')
+    exporter = default_export_generator.DefaultExportGenerator()
+    exporter.set_specification_from_model(mock_t2r_model)
+
+    # Export trained serving estimator.
+    estimator_exporter = tf_estimator.Estimator(
+        model_fn=mock_t2r_model.model_fn,
+        config=tf_estimator.RunConfig(model_dir=model_dir))
+
+    serving_input_receiver_fn = (
+        exporter.create_serving_input_receiver_numpy_fn())
+    exported_savedmodel_path = estimator_exporter.export_saved_model(
+        export_dir_base=model_dir,
+        serving_input_receiver_fn=serving_input_receiver_fn,
+        checkpoint_path=tf.train.latest_checkpoint(model_dir))
+
+    # Load trained and exported serving estimator, run prediction and assert
+    # it is the same as before exporting.
+    feed_predictor_fn = contrib_predictor.from_saved_model(
+        exported_savedmodel_path)
+    mock_input_generator = mocks.MockInputGenerator(batch_size=BATCH_SIZE)
+    features, labels = mock_input_generator.create_numpy_data()
+    for pos, value in enumerate(prediction_ref):
+      actual = feed_predictor_fn({'x': features[pos, :].reshape(
+          1, -1)})['logit'].flatten()
+      predicted = value['logit'].flatten()
+      np.testing.assert_almost_equal(
+          actual=actual, desired=predicted, decimal=4)
+      if labels[pos] > 0:
+        self.assertGreater(predicted[0], 0)
+      else:
+        self.assertLess(predicted[0], 0)
+
+  @parameterized.parameters((True,), (False,))
+  def test_create_serving_input_receiver_tf_example(self, multi_dataset):
+    (model_dir, mock_t2r_model,
+     prediction_ref) = self._train_and_eval_reference_model(
+         'tf_example', multi_dataset=multi_dataset)
+
+    # Now we can actually export our serving estimator.
+    estimator_exporter = tf_estimator.Estimator(
+        model_fn=mock_t2r_model.model_fn,
+        config=tf_estimator.RunConfig(model_dir=model_dir))
+
+    exporter = default_export_generator.DefaultExportGenerator()
+    exporter.set_specification_from_model(mock_t2r_model)
+    serving_input_receiver_fn = (
+        exporter.create_serving_input_receiver_tf_example_fn())
+    exported_savedmodel_path = estimator_exporter.export_saved_model(
+        export_dir_base=model_dir,
+        serving_input_receiver_fn=serving_input_receiver_fn,
+        checkpoint_path=tf.train.latest_checkpoint(model_dir))
+
+    # Now we can load our exported estimator graph, there are no dependencies
+    # on the model_fn or preprocessor anymore.
+    feed_predictor_fn = contrib_predictor.from_saved_model(
+        exported_savedmodel_path)
+    mock_input_generator = mocks.MockInputGenerator(batch_size=BATCH_SIZE)
+    features, labels = mock_input_generator.create_numpy_data()
+    for pos, value in enumerate(prediction_ref):
+      # We have to create our serialized tf.Example proto.
+      example = tf.train.Example()
+      example.features.feature['measured_position'].float_list.value.extend(
+          features[pos])
+      serialized_example = np.array(example.SerializeToString()).reshape(1,)
+      if multi_dataset:
+        feed_dict = {
+            'input_example_dataset1': serialized_example,
+            'input_example_dataset2': serialized_example
+        }
+      else:
+        feed_dict = {
+            'input_example_tensor': serialized_example
+        }
+      actual = feed_predictor_fn(feed_dict)['logit'].flatten()
+      predicted = value['logit'].flatten()
+      np.testing.assert_almost_equal(
+          actual=actual, desired=predicted, decimal=4)
+      if labels[pos] > 0:
+        self.assertGreater(predicted[0], 0)
+      else:
+        self.assertLess(predicted[0], 0)
+
+if __name__ == '__main__':
+  tf.test.main()
---
+++ tensor2robot/hooks/async_export_hook_builder.py
@@ -0,0 +1,134 @@
+# coding=utf-8
+# Copyright 2022 The Tensor2Robot Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Hook builders for TD3 distributed training with SavedModels."""
+
+import os
+import tempfile
+from typing import Text, List, Callable, Optional
+
+import gin
+from tensor2robot.export_generators import abstract_export_generator
+from tensor2robot.export_generators import default_export_generator
+from tensor2robot.hooks import checkpoint_hooks
+from tensor2robot.hooks import hook_builder
+from tensor2robot.models import model_interface
+from tensor2robot.proto import t2r_pb2
+from tensor2robot.utils import tensorspec_utils
+from tensorflow.compat.v1 import estimator as tf_estimator
+import tensorflow.compat.v1 as tf  # tf
+
+from tensorflow.contrib import tpu as contrib_tpu
+
+CreateExportFnType = Callable[[
+    model_interface.ModelInterface,
+    tf_estimator.Estimator,
+    abstract_export_generator.AbstractExportGenerator,
+], Callable[[Text, int], Text]]
+
+
+def default_create_export_fn(
+    t2r_model,
+    estimator,
+    export_generator
+):
+  """Create an export function for a device type.
+
+  Args:
+    t2r_model: A T2RModel instance.
+    estimator: The estimator used for training.
+    export_generator: An export generator.
+
+  Returns:
+    A callable function which exports a saved model and returns the path.
+  """
+
+  in_feature_spec = t2r_model.get_feature_specification_for_packing(
+      mode=tf_estimator.ModeKeys.PREDICT)
+  in_label_spec = t2r_model.get_label_specification_for_packing(
+      mode=tf_estimator.ModeKeys.PREDICT)
+  t2r_assets = t2r_pb2.T2RAssets()
+  t2r_assets.feature_spec.CopyFrom(in_feature_spec.to_proto())
+  t2r_assets.label_spec.CopyFrom(in_label_spec.to_proto())
+
+  def _export_fn(export_dir, global_step):
+    """The actual closure function creating the exported model and assets."""
+    # Create additional assets for the exported models
+    t2r_assets.global_step = global_step
+    tmpdir = tempfile.mkdtemp()
+    t2r_assets_filename = os.path.join(tmpdir,
+                                       tensorspec_utils.T2R_ASSETS_FILENAME)
+    tensorspec_utils.write_t2r_assets_to_file(t2r_assets, t2r_assets_filename)
+    assets = {
+        tensorspec_utils.T2R_ASSETS_FILENAME: t2r_assets_filename,
+    }
+    return estimator.export_saved_model(
+        export_dir_base=export_dir,
+        serving_input_receiver_fn=export_generator
+        .create_serving_input_receiver_numpy_fn(),
+        assets_extra=assets)
+
+  return _export_fn
+
+
+@gin.configurable
+class AsyncExportHookBuilder(hook_builder.HookBuilder):
+  """Creates hooks for exporting for cpu and tpu for serving.
+
+  Attributes:
+    export_dir: Directory to output the latest models.
+    save_secs: Interval to save models, and copy the latest model from
+      `export_dir` to `lagged_export_dir`.
+    num_versions: Number of model versions to save in each directory
+    export_generator: The export generator used to generate the
+      serving_input_receiver_fn.
+  """
+
+  def __init__(
+      self,
+      export_dir,
+      save_secs = 90,
+      num_versions = 3,
+      create_export_fn = default_create_export_fn,
+      export_generator = None,
+  ):
+    super(AsyncExportHookBuilder, self).__init__()
+    self._save_secs = save_secs
+    self._num_versions = num_versions
+    self._export_dir = export_dir
+    self._create_export_fn = create_export_fn
+    if export_generator is None:
+      self._export_generator = default_export_generator.DefaultExportGenerator()
+    else:
+      self._export_generator = export_generator
+
+  def create_hooks(
+      self,
+      t2r_model,
+      estimator,
+  ):
+    self._export_generator.set_specification_from_model(t2r_model)
+    return [
+        contrib_tpu.AsyncCheckpointSaverHook(
+            save_secs=self._save_secs,
+            checkpoint_dir=estimator.model_dir,
+            listeners=[
+                checkpoint_hooks.CheckpointExportListener(
+                    export_fn=self._create_export_fn(t2r_model, estimator,
+                                                     self._export_generator),
+                    num_versions=self._num_versions,
+                    export_dir=self._export_dir)
+            ])
+    ]
